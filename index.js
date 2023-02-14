ecd782ddd079be09b6143c649befb25e2002aa22
ELIAS-1829 <121119836+ELIAS-1829@users.noreply.github.com>
2023-02-14 11:11:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const imagen = 'logo.jpg';
+    const titulo = 'Estos son nuestros términos y condiciones de ,La  pagina para la parte de los troyanos y todas las demas  nosotro no nos aremos cargo del mal uso que se le de a los virus ,si algun dispositivo movil resulta afectado con el contenido de la pagina no no asemos cargo';                                 Swal.fire({                                                    imageUrl: imagen,                                          imageAlt: 'Imagen de un tercero',
+        title: 'BIENVENIDO A LG REYNA',
+        text: titulo
+      
+        
+    
+        
+    });
